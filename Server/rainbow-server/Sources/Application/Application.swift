3d6550293ae3cda5d5b46a47154cc7d8c64c0fc8
David-Okun <david.okun@ibm.com>
2018-05-03 19:30:12 -0500

    Basic iOS structure plus a docker run script for the server

--- Server/rainbow-server/Sources/Application/Application.swift
+++ Server/rainbow-server/Sources/Application/Application.swift
@@ -13,19 +13,33 @@ import IBMPushNotifications
 public let projectPath = ConfigurationManager.BasePath.project.path
 public let health = Health()
 
+enum ServiceInitializationError: Error {
+    case CloudantError(String)
+    case PushNotificationError(String)
+}
+
 public class ApplicationServices {
     // Initialize services
-    public var couchDBService: CouchDBClient
-    public let pushNotificationService: PushNotifications?
+    public var couchDBService: CouchDBClient?
+    //public let pushNotificationService: PushNotifications?
 
     public init(cloudEnv: CloudEnv) throws {
         // Run service initializers
         do {
             couchDBService = try initializeServiceCloudant(cloudEnv: cloudEnv)
-            pushNotificationService = try initializeServicePush(cloudEnv: cloudEnv)
+            //pushNotificationService = try initializeServicePush(cloudEnv: cloudEnv)
+        } catch ServiceInitializationError.CloudantError(let reason) {
+            Log.error("Error setting up Cloudant: \(reason)")
+            couchDBService = nil
+            //pushNotificationService = nil
+        } catch ServiceInitializationError.PushNotificationError(let reason) {
+            Log.error("Error setting up Push Notifications: \(reason)")
+            //pushNotificationService = nil
+            //couchDBService = CouchDBClient(connectionProperties: ConnectionProperties(host: "localhost", port: 5984, secured: false))
         } catch {
-            couchDBService = CouchDBClient(connectionProperties: ConnectionProperties(host: "localhost", port: 5984, secured: false))
-            pushNotificationService = nil
+            //pushNotificationService = nil
+            couchDBService = nil
+            Log.error("Unhandled exception during service init.")
         }
     }
 }
---
+++ iOS/rainbow/Controller/Camera/CameraController.swift
@@ -0,0 +1,38 @@
+//
+//  CameraController.swift
+//  rainbow
+//
+//  Created by David Okun IBM on 5/3/18.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import Foundation
+import Lumina
+
+class CameraController: LuminaViewController {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.delegate = self
+        self.streamingModelTypes = [RainbowModel_1820023450()]
+        self.setShutterButton(visible: false)
+        self.setTorchButton(visible: true)
+        self.setCancelButton(visible: false)
+        self.setSwitchButton(visible: false)
+    }
+}
+
+extension CameraController: LuminaDelegate {
+    func streamed(videoFrame: UIImage, with predictions: [LuminaRecognitionResult]?, from controller: LuminaViewController) {
+        guard let bestName = predictions?.first?.predictions?.first?.name else {
+            return
+        }
+        guard let bestConfidence = predictions?.first?.predictions?.first?.confidence else {
+            return
+        }
+        if bestConfidence >= 0.9 {
+            self.textPrompt = "Detected: \(bestName) (\(bestConfidence))"
+        } else {
+            self.textPrompt = ""
+        }
+    }
+}
