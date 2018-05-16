d54ad840ad2901c0dd59784cc7996aa681e25a9c
Anton McConville <anton@hickory.ca>
2018-05-16 16:02:58 -0400

    Adding booklet files and images
    This commit establishes a multi view booklet reading from a json file on the file system ( which will act as the default ). I want to add another update that will  read from a json file stored raw on github. I want to remove the navigation bar in this view. I need to add links to the patterns, and I want to tweak the images and spacing eventually.

---
+++ iOS/rainbow/Controller/Booklet/BookletCoverController.swift
@@ -0,0 +1,80 @@
+//
+//  CoverViewController.swift
+//  rainbow
+//
+//  Created by Anton McConville on 2018-05-11.
+//  Copyright © 2018 IBM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BookletCoverController: BookletBaseController {
+    
+    @IBOutlet var contentImageView: UIImageView?
+    @IBOutlet var pageTitleView: UILabel?
+    @IBOutlet var subtitleView: UILabel?
+    
+    @IBAction func openLink(_ sender: UIButton) {
+        performSegue(withIdentifier: "webkitSegue", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "webkitSegue" {
+            guard let navController = segue.destination as? UINavigationController else {
+                return
+            }
+            guard let webview = navController.topViewController as? AssetViewController else {
+                return
+            }
+            webview.link = self.link
+        }
+    }
+    
+    // MARK: - Variables
+
+    var link: String = ""
+    
+    var image: UIImage = UIImage() {
+        didSet {
+            if let imageView = contentImageView {
+                imageView.image = image
+            }
+        }
+    }
+    
+    var titleString: String = "" {
+        didSet {
+            if let titleView = pageTitleView {
+                titleView.text = titleString
+            }
+        }
+    }
+    
+    var subTitleString: String = "" {
+        didSet {
+            if let subtitleView = subtitleView {
+                subtitleView.text = titleString
+            }
+        }
+    }
+    
+    
+    var linkString: String = "" {
+        didSet {
+            link = linkString
+        }
+    }
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //        contentImageView!.image = image
+                pageTitleView!.text = titleString
+        //        subtitleView!.text = subTitleString
+        //        statement?.text = statementString
+        //        link = linkString
+        //
+        //        button?.layer.cornerRadius = 20
+        //        button?.clipsToBounds = true
+    }
+}
