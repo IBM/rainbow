5e290a2c0cec08096ccc0bf64b8ec661541581d4
David Okun <david@okun.io>
2018-06-01 11:58:40 -0500

    Added model file to iOS project

---
+++ iOS/rainbow/Model/UserCount.swift
@@ -0,0 +1,13 @@
+//
+//  UserCount.swift
+//  Application
+//
+//  Created by Sanjeev Ghimire on 6/1/18.
+//
+
+import Foundation
+
+struct UserCount: Codable {
+    var totalUsers: Int
+    var totalUsersCompletingGame: Int
+}
