06b74642fdebf5c52d15bda641a63505bcf48052
David Okun <david@okun.io>
2019-05-16 13:12:52 -0500

    added workaround for models made by watson visual recognition that don't handle A12 chip effectively.

--- iOS/rainbow/Model/KituraServerCredentials.swift
+++ iOS/rainbow/Model/KituraServerCredentials.swift
@@ -86,7 +86,14 @@ class VisualRecognitionUpdate {
                 }
             }
             if models.count == 0 {
-                return completion(WatsonMLModel_263558779().model, nil)
+                if #available(iOS 12.0, *), UIDevice.usesA12 {
+                    let configuration = MLModelConfiguration()
+                    configuration.computeUnits = .cpuAndGPU
+                    let configuredModel = try? MLModel(contentsOf: WatsonMLModel_263558779.urlOfModelInThisBundle, configuration: configuration)
+                    return completion(configuredModel, nil)
+                } else {
+                    return completion(WatsonMLModel_263558779().model, nil)
+                }
             } else {
                 return completion(models.first, nil)
             }
@@ -108,6 +115,61 @@ class VisualRecognitionUpdate {
                 }
             }
             
+        }
+    }
+}
+
+fileprivate extension UIDevice {
+    enum DevicePlatform {
+        case other
+        case iPhone6S
+        case iPhone6SPlus
+        case iPhone7
+        case iPhone7Plus
+        case iPhone8
+        case iPhone8Plus
+        case iPhoneX
+        case iPhoneXS
+        case iPhoneXSMax
+        case iPhoneXR
+        case simulator
+    }
+    
+    static var usesA12: Bool {
+        return platform == .iPhoneXR ||
+            platform == .iPhoneXS ||
+            platform == .iPhoneXSMax
+    }
+    
+    static var platform: DevicePlatform {
+        var sysinfo = utsname()
+        uname(&sysinfo)
+        let platform = String(bytes: Data(bytes: &sysinfo.machine, count: Int(_SYS_NAMELEN)), encoding: .ascii)!.trimmingCharacters(in: .controlCharacters)
+        switch platform {
+        case "x86_64":
+            return .simulator
+        case "iPhone11,2":
+            return .iPhoneXS
+        case "iPhone11,4", "iPhone11,6":
+            return .iPhoneXSMax
+        case "iPhone11,8":
+            return .iPhoneXR
+        case "iPhone10,1", "iPhone10,4":
+            return .iPhone8
+        case "iPhone10,2", "iPhone10,5":
+            return .iPhone8Plus
+        case "iPhone10,3", "iPhone10,6":
+            return .iPhoneX
+        case "iPhone9,2", "iPhone9,4":
+            return .iPhone7Plus
+        case "iPhone9,1", "iPhone9,3":
+            return .iPhone7
+        case "iPhone8,2":
+            return .iPhone6SPlus
+        case "iPhone8,1":
+            return .iPhone6S
+        default:
+            return .other
         }
     }
 }
